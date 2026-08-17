0f579b71875beba5414e1cb878dfbbfb72a50f24
_justParrot <contact@justparrot.me>
2026-08-17 11:13:05 +0200

    Update cli.js

--- src/cli.js
+++ src/cli.js
@@ -2,4 +2,6 @@ const { createServer } = require("./services/serverService.js")
 
 const serverName = process.argv[2];
 
-createServer(serverName)
+const result = createServer(serverName)
+
+console.log(JSON.stringify(result, null, 2));
